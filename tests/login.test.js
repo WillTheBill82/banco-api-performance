2db898cb0437c0bb79bebb364ea06c8fc6790195
WillTheBill82 <williamrodrigues198231@gmail.com>
2026-09-08 21:59:22 -0300

    Thresholds e check

--- tests/login.test.js
+++ tests/login.test.js
@@ -3,7 +3,12 @@ import { sleep, check } from 'k6';
 
 export const options = {
     iterations: 10,
-}; 
+    thresholds: {
+        http_req_duration : ['p(90)<10', 'max<1'],
+        http_req_failed : ['rate<0.01'],
+        
+    }
+};
 
 export default function () {
     const url = 'http://localhost:3000/login';
